b63459e7c5740c480914505586c665f248f79fd2
Phebesue <62977906+Phebesue@users.noreply.github.com>
2020-09-28 14:00:30 -0400

    change app.jsx to app.js. Update user model to include role. Update package.json to launch app.js

--- models/user.js
+++ models/user.js
@@ -17,6 +17,10 @@ module.exports =  (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false
       },
+      userRole: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
     })
     return User;
   }
