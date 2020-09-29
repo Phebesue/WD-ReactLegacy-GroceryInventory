ebdf615e9098f0f6d7aadb99d1a5061ae2e24b1a
Phebesue <62977906+Phebesue@users.noreply.github.com>
2020-09-29 14:01:16 -0400

    add enums to models. Attempt too add db associations to db.js. Add Admin restrictions on user get all & delete

--- models/vendor.js
+++ models/vendor.js
@@ -1,37 +1,37 @@
-module.exports =  (sequelize, DataTypes) => {
-    const Vendor = sequelize.define("vendor", {
-      vendorName: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      website: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      address: {
-        type: DataTypes.STRING,
-        allowNull: true,
-             },
-      city: {
-        type: DataTypes.STRING,
-        allowNull: true
-      },
-      state: {
-        type: DataTypes.STRING,
-        allowNull: true
-      },
-      zipcode: {
-        type: DataTypes.STRING,
-        allowNull: true
-      },
-      vendorNotes: {
-        type: DataTypes.STRING,
-        allowNull: true
-      },
-      userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-          }
-    })
-    return Vendor;
-  }
+module.exports = (sequelize, DataTypes) => {
+  const Vendor = sequelize.define("vendor", {
+    vendorName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    website: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    zipcode: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    vendorNotes: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
+  return Vendor;
+};
